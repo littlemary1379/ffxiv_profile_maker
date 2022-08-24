6846ae2ea33473c134f8eddf8bc9c705c090188a
littlemary1379 <littlemary1379@gmail.com>
2022-08-24 17:53:17 +0900

    image structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
+import imageTest from './img/ffxiv_sample_back.png';
 
 function App() {
   return (
     <div className="App">
+      {/*section_title start*/}
       <div className="title_area">
         <p className='title'>FFXIV 시트메이커</p>
       </div>
+      {/*section_title end*/}
+
+      {/*section_image start*/}
       <div className='sheet_section'>
         <div className='section_image'>
-          <image src='/img/ffxiv_sample_back.png'/>
+          <img src = {imageTest}/>
+        </div>
+        <div className='section_image_gradient'>
+
+        </div>
+        <div className='section_image_pvp'>
+
         </div>
       </div>
+      {/*section_image end*/}
     </div>
   );
 }
