a78934ab992adc9359a44754d19c489a2d1856ef
littlemary1379 <littlemary1379@gmail.com>
2022-08-24 16:56:52 +0900

    structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="title_area">
+        <p className='title'>FFXIV 시트메이커</p>
+      </div>
+      <div className='sheet_section'>
+        <div className='section_image'>
+          <image src='/img/ffxiv_sample_back.png'/>
+        </div>
+      </div>
     </div>
   );
 }
